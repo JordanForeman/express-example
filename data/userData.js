c6a703d406546e316961ebc5e4c54a31f0f704ff
Jordan Foreman <thejordanforeman@gmail.com>
2015-09-14 19:10:11 -0500

    create mock data

---
+++ data/userData.js
@@ -0,0 +1,44 @@
+module.exports = [
+  {
+    "name": "Erin Craig",
+    "email": "erincraig@insuron.com",
+    "age": 21,
+    "company": "GEEKULAR"
+  },
+  {
+    "name": "English Dyer",
+    "email": "englishdyer@geekular.com",
+    "age": 34,
+    "company": "OLYMPIX"
+  },
+  {
+    "name": "Stephenson Ortega",
+    "email": "stephensonortega@olympix.com",
+    "age": 36,
+    "company": "GEEKMOSIS"
+  },
+  {
+    "name": "Sheryl Bryan",
+    "email": "sherylbryan@geekmosis.com",
+    "age": 34,
+    "company": "CUIZINE"
+  },
+  {
+    "name": "Florine Oneil",
+    "email": "florineoneil@cuizine.com",
+    "age": 22,
+    "company": "SENMAO"
+  },
+  {
+    "name": "Jo Campos",
+    "email": "jocampos@senmao.com",
+    "age": 20,
+    "company": "BISBA"
+  },
+  {
+    "name": "Orr Hull",
+    "email": "orrhull@bisba.com",
+    "age": 21,
+    "company": "COMTEXT"
+  }
+];
