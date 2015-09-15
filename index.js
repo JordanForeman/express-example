7ad973e94078029a1cd8a1049c3851c677b380c5
Jordan Foreman <thejordanforeman@gmail.com>
2015-09-14 19:04:04 -0500

    Basic app.send

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var express = require('express'),
 
 var app = express(); // Initialize Express Application instance
 
+app.get('/', function(request, response){
+  response.send('Test Successful!');
+});
+
 // After configuring the Express App, launch an HTTP Server to handle requests
 app.listen(config.port, function(){
   console.log('Listening on port ' + config.port);
